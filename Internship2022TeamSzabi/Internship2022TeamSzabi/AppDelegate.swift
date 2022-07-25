626df1649b62b1e77135ad75f7711e52cb92f26d
Szabolcs Orban <szabolcs.orban@endava.com>
2022-07-25 10:04:01 +0300

    initial comit

---
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Internship2022TeamSzabi
+//
+//  Created by Szabolcs Orban on 25.07.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Internship2022TeamSzabi
+//
+//  Created by Szabolcs Orban on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
