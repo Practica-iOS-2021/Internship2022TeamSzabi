e872f4bbbdfbee4501c6f564a1f9ccb0ee7b0d73
CristianKuics <aSS>
2022-07-27 16:50:22 +0300

    Changed : 	- for in forEach 			- made function private 			- removed "self" keyword and improved my code guidelines knowledge              		- changed force cast to optional at function level

--- Internship2022TeamSzabi/Internship2022TeamSzabi/Views/TabBarController/TabBarController.swift
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/Views/TabBarController/TabBarController.swift
@@ -13,35 +13,34 @@ class TabBarController: UITabBarController {
         super.viewDidLoad()
         
         // MARK: - Instantiate view controllers
-        let gradesController = createNavController(vc: GradesViewController(), tabBarImage: UIImage(named: "grades-tabbar-icon")!)
-        let coursesController = createNavController(vc: CoursesViewController(), tabBarImage: UIImage(named: "courses-tabbar-icon")!)
-        let profileController = createNavController(vc: ProfileViewController(), tabBarImage: UIImage(named: "profile-tabbar-icon")!)
+        let gradesController = createNavController(vc: GradesViewController(), tabBarImage: UIImage(named: "grades-tabbar-icon"))
+        let coursesController = createNavController(vc: CoursesViewController(), tabBarImage: UIImage(named: "courses-tabbar-icon"))
+        let profileController = createNavController(vc: ProfileViewController(), tabBarImage: UIImage(named: "profile-tabbar-icon"))
         
         // MARK: - Asigning view controllers
-        self.setViewControllers([gradesController, coursesController, profileController], animated: false)
+        setViewControllers([gradesController, coursesController, profileController], animated: false)
         
         // MARK: - Setting CoursesView as initial display view
         self.selectedIndex = 1
         
         // MARK: - TabBar configuration
-        self.tabBar.itemPositioning = .fill;
-        self.tabBar.tintColor = UIColor(named: "TabBarSelectedColor")
-        self.tabBar.unselectedItemTintColor = UIColor(named: "TabBarColor")
-        self.tabBar.backgroundColor = .white
+        tabBar.itemPositioning = .fill;
+        tabBar.tintColor = UIColor(named: "TabBarSelectedColor")
+        tabBar.unselectedItemTintColor = UIColor(named: "TabBarColor")
+        tabBar.backgroundColor = .white
         
-        guard let tabItems = tabBar.items else { return }
-        
-        for x in 0..<tabItems.count {
-            tabItems[x].imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
-            tabItems[x].title = ""
+        tabBar.items?.forEach { tabItems in
+            //Insets are needed centering items for Iphone 11 and above
+            tabItems.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
+            tabItems.title = ""
         }
     }
 }
 
-extension UITabBarController {
+extension TabBarController {
     
     // MARK: - UINavigationController
-    func createNavController(vc: UIViewController, tabBarImage: UIImage) -> UINavigationController {
+    private func createNavController(vc: UIViewController, tabBarImage: UIImage?) -> UINavigationController {
         let viewController = vc
         let navController = UINavigationController(rootViewController: viewController)
         navController.tabBarItem.image = tabBarImage
