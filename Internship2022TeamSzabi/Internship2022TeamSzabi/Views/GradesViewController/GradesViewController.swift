12795e11304c061e71d664e5e5379f775224f4b3
CristianKuics <aSS>
2022-07-27 14:00:11 +0300

    Created TabBarController -created initial display view -created UITabBarController logic -created mock ViewControllers -added images.assets -added color.assets

---
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/Views/GradesViewController/GradesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GradesViewController.swift
+//  Internship2022TeamSzabi
+//
+//  Created by Cristian Vasile Kui-cs on 26.07.2022.
+//
+
+import UIKit
+
+class GradesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Grades"
+    }
+}
