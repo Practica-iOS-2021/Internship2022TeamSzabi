82194a10f9d33aa3de1b0548dc761f3b36fe00ed
ahuzmeza <alinhuzmezan@yahoo.com>
2022-07-30 13:53:17 +0300

    Improved validations and code cleanup

--- Internship2022TeamSzabi/Internship2022TeamSzabi/Views/Register/RegisterViewController.swift
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/Views/Register/RegisterViewController.swift
@@ -19,69 +19,86 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupAppearence()
     }
-
+    
     @IBAction private func emailChanged(_ sender: Any) {
-        validateIfFormFilled()
-        guard let input = emailTextField.text else { return }
-        checkForError(text: input, validationFunction: validateEmail)
+        validateForm()
     }
     @IBAction func nameChanged(_ sender: Any) {
-        validateIfFormFilled()
+        validateForm()
     }
     @IBAction private func passwordChanged(_ sender: Any) {
-        validateIfFormFilled()
-        guard let input = passwordTextfield.text else { return }
-        checkForError(text: input, validationFunction: validatePassword)
+        validateForm()
     }
     @IBAction private func personalIDChanged(_ sender: Any) {
-        validateIfFormFilled()
-        guard let input = personalIDTextfield.text else { return }
-        checkForError(text: input, validationFunction: validatePersonalID)
+        validateForm()
     }
     @IBAction private func studentIDChanged(_ sender: Any) {
-        validateIfFormFilled()
+        validateForm()
     }
     @IBAction private func submitAction(_ sender: Any) {
         handleFormValidation()
-    }
-
-    private func setupAppearence() {
-        // for each view in subviews of type 'UITextField':
-        // set style to said view
-        formView.subviews.forEach { view in
-            guard let textField = view as? UITextField else { return }
-            addStyleToTextfield(textField: textField)
-        }
-
-        addStyleToButton(button: registerButton)
-        passwordTextfield.isSecureTextEntry = true
-        errorLabel.isHidden = true
-        registerButton.isEnabled = false
-    }
-
-    private func handleFormValidation() {
-        guard let errorText = errorLabel.text else { return }
-        guard errorText.isEmpty else { return }
-
-        // all validations passed
-        handleRegister()
     }
 
     private func handleRegister() {
         print("ready to register")
     }
 
-    func checkForError(text: String, validationFunction: (String) -> String?) {
+    private func handleFormValidation() {
+        guard validateForm() else { return }
+        
+        // all validations passed
+        handleRegister()
+    }
+
+    // MARK: - Check against invalidations in form
+    @discardableResult
+    private func validateForm() -> Bool {
+        // supose form initially valid
+        var valid = true
+
+        // 1) if not all textFields filled
+        formView.subviews.forEach { view in
+            if let textField = view as? UITextField {
+                if textField.text?.isEmpty ?? true {
+                    valid = false
+                }
+            }
+        }
+
+        // get text from need-be valid textFields
+        guard let emailText = emailTextField.text,
+              let personalIDText = personalIDTextfield.text,
+              let passwordText = passwordTextfield.text else { return false }
+
+        // 2) if said texts aren't valid
+        guard checkInputError(text: emailText,
+                              validationFunction: validateEmail),
+              checkInputError(text: personalIDText,
+                              validationFunction: validatePersonalID),
+              checkInputError(text: passwordText,
+                              validationFunction: validatePassword)
+        else { return false }
+        
+        // enable/disable 'register' button and return verdict
+        registerButton.isEnabled = valid
+        return valid
+    }
+
+    // MARK: - Validation methods
+    // checkInputError uses given 'text'
+    // validated by given 'validationFunction'
+    func checkInputError(text: String, validationFunction: (String) -> String?) -> Bool {
         if let foundError = validationFunction(text) {
             errorLabel.text = foundError
             errorLabel.isHidden = false
-            return
+            return false
         }
         errorLabel.text = ""
         errorLabel.isHidden = true
+        return true
     }
 
     func validateEmail(_ email: String) -> String? {
@@ -94,7 +111,7 @@ class RegisterViewController: UIViewController {
         // valid email
         return nil
     }
-
+    
     func validatePassword(_ password: String) -> String? {
         let passwordRegEx = "^(?=.{8,})(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$"
         let emailPred = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
@@ -114,7 +131,7 @@ class RegisterViewController: UIViewController {
         // valid password
         return nil
     }
-
+    
     func validatePersonalID(_ personalID: String) -> String? {
         let personalIDRegEx = "^([a-zA-Z0-9]+(\\s|-))*[a-zA-Z0-9]+$"
         let emailPred = NSPredicate(format: "SELF MATCHES %@", personalIDRegEx)
@@ -130,26 +147,29 @@ class RegisterViewController: UIViewController {
         return nil
     }
 
-
-    private func validateIfFormFilled() {
-        let firstEmpty = formView.subviews.first { view in
-            if let textField = view as? UITextField {
-                return textField.text?.isEmpty ?? true
-            }
-            return false
-        }
-
-        // enable/disable 'register' button
-        registerButton.isEnabled = firstEmpty == nil
-    }
-
+    // MARK: - Alert for errors
     func alertError(_ error: String) {
         let dialogMessage = UIAlertController(title: "Error", message: error, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default)
         dialogMessage.addAction(okAction)
         present(dialogMessage, animated: true, completion: nil)
     }
-
+    
+    // MARK: - Appearence methods
+    private func setupAppearence() {
+        // for each view in subviews of type 'UITextField':
+        // set style to said view
+        formView.subviews.forEach { view in
+            guard let textField = view as? UITextField else { return }
+            addStyleToTextfield(textField: textField)
+        }
+        
+        addStyleToButton(button: registerButton)
+        passwordTextfield.isSecureTextEntry = true
+        errorLabel.isHidden = true
+        registerButton.isEnabled = false
+    }
+    
     func addStyleToButton(button: UIButton) {
         // shadow
         button.layer.shadowColor = UIColor.black.cgColor
@@ -157,7 +177,7 @@ class RegisterViewController: UIViewController {
         button.layer.shadowRadius = 2
         button.layer.shadowOpacity = 0.2
     }
-
+    
     func addStyleToTextfield(textField: UITextField) {
         // border bottom
         let bottomLine = CALayer()
@@ -166,4 +186,4 @@ class RegisterViewController: UIViewController {
         textField.borderStyle = UITextField.BorderStyle.none
         textField.layer.addSublayer(bottomLine)
     }
-}
+} // eOF RegisterViewController
