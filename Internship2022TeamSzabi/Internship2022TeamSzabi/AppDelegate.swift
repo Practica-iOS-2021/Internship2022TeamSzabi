c0f2c009dd84816252b08a7193becbc3822faf5a
CristianKuics <aSS>
2022-08-09 17:55:35 +0300

    Changed rootController to Splashscreen

--- Internship2022TeamSzabi/Internship2022TeamSzabi/AppDelegate.swift
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // MARK: - Assigning initial display view
         window = UIWindow()
         window?.frame = UIScreen.main.bounds
-        window?.rootViewController = QuizViewController()
+        window?.rootViewController = SplashViewController()
         window?.makeKeyAndVisible()
         
         return true
