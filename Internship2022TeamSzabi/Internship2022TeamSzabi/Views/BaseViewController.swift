43ba33e5444b0d3b72c59f2a3835783896bfe5e5
ahuzmeza <alinhuzmezan@yahoo.com>
2022-08-11 12:51:06 +0300

    added spinner

---
+++ Internship2022TeamSzabi/Internship2022TeamSzabi/Views/BaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BaseViewController.swift
+//  Internship2022TeamSzabi
+//
+//  Created by Alin Huzmezan on 8/11/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    private var baseView: UIView?
+    private var nextView: UIView?
+    private var button: UIButton?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    func loadSpinner() {
+        baseView = UIView(frame: view.bounds)
+        baseView?.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        
+        let baseView = UIActivityIndicatorView(style: UIActivityIndicatorView.Style.large)
+        baseView.center = baseView.center
+        baseView.startAnimating()
+        baseView.addSubview(baseView)
+        view.addSubview(baseView)
+    }
+    
+    func removeSpinner() {
+        baseView?.removeFromSuperview()
+        baseView = nil
+    }
+}
